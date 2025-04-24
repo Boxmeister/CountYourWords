3bfeb91eeef07685e156f827266f074a8a0d045c
Boxmeister <yozora@live.nl>
2025-04-24 11:54:24 +0200

    soter sorts counted list

--- CountYourWords/Interfaces/ISorter.cs
+++ CountYourWords/Interfaces/ISorter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CountYourWords.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace CountYourWords.Interfaces
 {
     interface ISorter
     {
-        List<string> Sort(List<string> words);
+        List<WordCount> Sort(List<WordCount> words);
     }
 }
--- CountYourWords/Services/Sorter.cs
+++ CountYourWords/Services/Sorter.cs
@@ -1,4 +1,5 @@
 ﻿using CountYourWords.Interfaces;
+using CountYourWords.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,22 @@ namespace CountYourWords.Services
 {
     public class Sorter : ISorter
     {
-        public List<string> Sort(List<string> words)
+        public List<WordCount> Sort(List<WordCount> countedWordList)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < countedWordList.Count - 1; i++)
+            {
+                for (int j = 0; j < countedWordList.Count - i - 1; j++)
+                {
+                    if (string.CompareOrdinal(countedWordList[j].Word, countedWordList[j + 1].Word) > 0)
+                    {
+                        var temp = countedWordList[j];
+                        countedWordList[j] = countedWordList[j + 1];
+                        countedWordList[j + 1] = temp;
+                    }
+                }
+            }
+            return countedWordList;
         }
+
     }
 }
